722a0f2959427b4b56283f4d86049240a1003fb9
bmVsc29u <bmVsc29u@gmail.com>
2019-05-06 10:34:41 +0800

    fix: new lib setup issues

---
+++ projects/chakray/hero/src/hero/hero.tag.spec.ts
@@ -0,0 +1,5 @@
+describe('placeholde', () => {
+  it('is pending now', () => {
+    expect(1).toEqual(1);
+  });
+});
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,9 +1,15 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { TestBed, async } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      schemas: [NO_ERRORS_SCHEMA],
+      imports: [
+        HttpClientTestingModule
+      ],
       declarations: [
         AppComponent
       ],
