d3b0bcaf743b5d4b634a7fa328881f6940289970
bmVsc29u <bmVsc29u@gmail.com>
2019-05-07 00:42:06 +0800

    feat: add travis badge

---
+++ projects/chakray/hero/src/hero/badges.ts
@@ -0,0 +1,21 @@
+export const badges = [{
+  text: 'npm',
+  logo: 'assets/hero/npm.png',
+  base: 'https://www.npmjs.com/package/@'
+}, {
+  text: 'Github',
+  logo: 'assets/hero/github.png',
+  base: 'https://github.com/'
+}, {
+  text: 'Travis',
+  logo: 'assets/hero/travis.png',
+  base: 'https://travis-ci.org/'
+}, {
+  text: 'Angular',
+  logo: 'assets/hero/ng.png',
+  url: 'https://angular.io'
+}, {
+  text: 'bmVsc29u',
+  logo: 'https://avatars3.githubusercontent.com/u/46260795?s=460&v=4',
+  url: 'https://github.com/bmVsc29u'
+}];
--- projects/chakray/hero/src/hero/hero.tag.ts
+++ projects/chakray/hero/src/hero/hero.tag.ts
@@ -1,23 +1,6 @@
 import { Input, Component } from '@angular/core';
 import { Hero } from './hero';
-
-const badges = [{
-  text: 'npm',
-  logo: 'assets/hero/npm.png',
-  base: 'https://www.npmjs.com/package/@'
-}, {
-  text: 'Github',
-  logo: 'assets/hero/github.png',
-  base: 'https://github.com/'
-}, {
-  text: 'Angular',
-  logo: 'assets/hero/ng.png',
-  url: 'https://angular.io'
-}, {
-  text: 'bmVsc29u',
-  logo: 'https://avatars3.githubusercontent.com/u/46260795?s=460&v=4',
-  url: 'https://github.com/bmVsc29u'
-}];
+import { badges } from './badges';
 
 @Component({
   selector: 'ch-hero',
