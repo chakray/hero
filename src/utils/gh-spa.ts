2654df3f0cb6e81bbd5f956bd0d52b035f0ec3b8
bmVsc29u <bmVsc29u@gmail.com>
2019-05-11 01:19:46 +0800

    chore(demo): apply utils

--- src/utils/gh-spa.ts
+++
@@ -1,7 +0,0 @@
-(() => {
-  const { redirect } = sessionStorage;
-  delete sessionStorage.redirect;
-  if (redirect && redirect !== location.href) {
-    history.replaceState(null, null, redirect);
-  }
-})();
